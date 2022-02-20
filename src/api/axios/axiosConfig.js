0a0c9338e351d16067e86ccbbc7e79e8c0f8d25a
hoangdinh2411 <hoangdinh2411@gmail.com>
2022-02-20 20:10:28 +0100

    update

--- src/api/axios/axiosConfig.js
+++ src/api/axios/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000' || 'https://yumy-beauty-salon-server-app.herokuapp.com',
+  baseURL: 'http://localhost:5000',
   timeout:10000,
   headers: {
     "content-type": "application/json",
